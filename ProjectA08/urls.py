e7b2f1f7d850c3ab9ba79c1af9400e35ea94e3cb
Krish1903 <98330531+Krish1903@users.noreply.github.com>
2023-02-13 01:50:32 -0500

    made app

--- ProjectA08/urls.py
+++ ProjectA08/urls.py
@@ -18,8 +18,8 @@ from django.urls import path
 from django.urls import path,include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path('', include('ProjectA08.urls')),
-    path('ProjectA08/', include('ProjectA08.urls')),
+    path('/ProjectA08', include('ProjectA08.urls')),
+    path("admin/", admin.site.urls),
 ]
 
