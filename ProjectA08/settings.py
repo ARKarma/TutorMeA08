9565da3d394c7284bb320dfa6cfcb366d30b9e48
Krish1903 <98330531+Krish1903@users.noreply.github.com>
2023-02-13 02:09:26 -0500

    everything is fixed, minor changes for refactoring

--- ProjectA08/settings.py
+++ ProjectA08/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-%b7a3!0tmtb#m#d8n!)t_zjy!pvnfue2p@%g+n^djou^69wfii
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1', 'https://project-a-08.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost','127.0.0.1', 'project-a-08.herokuapp.com']
 
 
 # Application definition
@@ -117,3 +117,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+try:
+    if 'HEROKU' in os.environ:
+        import django_heroku
+        django_heroku.settings(locals())
+except ImportError:
+    found = False
