67d2002ca844c141b1785698bda13cb672749112
ARKarma <psb7wm@virginia.edu>
2023-03-01 15:28:34 -0500

    Made changes to use PostgreSQL

--- ProjectA08/settings.py
+++ ProjectA08/settings.py
@@ -135,6 +135,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
+
 try:
     if 'HEROKU' in os.environ:
         import django_heroku
