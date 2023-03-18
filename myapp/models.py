ff1851fe87ac98fc039f29169f5b2dc925cd386f
ARKarma <psb7wm@virginia.edu>
2023-03-18 04:49:36 -0400

    Made user model

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email= models.CharField(max_length=320)
+    STUDENT= 'STUDENT'
+    TUTOR= 'TUTOR'
+    USER_TYPE_CHOICES = [
+        (STUDENT, 'Student'),
+        (TUTOR, 'Tutor')
+    ]
+    user_type= models.CharField(
+        max_length=8,
+        choices=USER_TYPE_CHOICES,
+        default=STUDENT
+    )
+
