5419e307e9c6de8d62e7d4e4c82f216caaffebfd
ARKarma <psb7wm@virginia.edu>
2023-03-20 07:02:48 -0400

    edited test 1, seeing if it works in github

--- ProjectA08/tests/test_intial.py
+++ ProjectA08/tests/test_intial.py
@@ -1,9 +1,13 @@
+import pytest
 from django.urls import reverse
 from myapp.views import home
+from django.contrib.auth.models import AnonymousUser, User
+from django.test import RequestFactory
 
 @pytest.mark.django_db
 def test_home():
-    path= reverse("")
+    path= reverse("login")
     request= RequestFactory().get(path)
+    request.user = AnonymousUser()
     response = home(request)
     assert response.status_code == 200
