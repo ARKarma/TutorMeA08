5cfb2c3dbde39686a512c9f49bb958f3149e3b99
kjk5drb <kjk5drb@virginia.edu>
2023-03-20 17:32:38 -0400

    course in db test only

--- ProjectA08/tests/test_intial.py
+++ ProjectA08/tests/test_intial.py
@@ -14,12 +14,12 @@ def test_home_redirect():
     print(response['location'])
     assert response.status_code == 302
 
-@pytest.mark.django_db
-def test_course_create():
-    course = Course.objects.create(subject="AAS",catalog_number="3710",class_section="001",class_number= 12798,class_title="African Worlds, Life Stories",class_topic_formal_desc="",instructor="Shutt,Lisa Toccafondi",enrollment_capacity=16,meeting_days="R",meeting_time_start="14:00:00", meeting_time_end="16:30:00",term="1228",term_desc="2022 Fall")
-    assert course.subject == "AAS"
-    assert course.catalog_number == "3710"
-    assert course.class_number!=12799
+# @pytest.mark.django_db
+# def test_course_create():
+#     course = Course.objects.create(subject="AAS",catalog_number="3710",class_section="001",class_number= 12798,class_title="African Worlds, Life Stories",class_topic_formal_desc="",instructor="Shutt,Lisa Toccafondi",enrollment_capacity=16,meeting_days="R",meeting_time_start="14:00:00", meeting_time_end="16:30:00",term="1228",term_desc="2022 Fall")
+#     assert course.subject == "AAS"
+#     assert course.catalog_number == "3710"
+#     assert course.class_number!=12799
 
 @pytest.mark.django_db
 def test_course_in_database():
