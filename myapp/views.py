c7e15fb5a99ab6f8d47c563490e7a856f84eebd2
Krish1903 <98330531+Krish1903@users.noreply.github.com>
2023-03-20 02:49:57 -0400

    used bulk methods to improve efficiency of fetching courses

--- myapp/views.py
+++ myapp/views.py
@@ -5,31 +5,46 @@ from myapp.models import Course
 from django.db.models import Q
 
 
-def fetch_courses():
+def fetch_courses(page_size=100):
     url = 'https://api.devhub.virginia.edu/v1/courses'
-    data = requests.get(url).json()
-
     courses = []
-    for record in data['class_schedules']['records']:
-        course = Course(
-            subject=record[0],
-            catalog_number=record[1],
-            class_section=record[2],
-            class_number=record[3],
-            class_title=record[4],
-            class_topic_formal_desc=record[5],
-            instructor=record[6],
-            enrollment_capacity=record[7],
-            meeting_days=record[8],
-            meeting_time_start=record[9],
-            meeting_time_end=record[10],
-            term=record[11],
-            term_desc=record[12]
-        )
-        course.save()  
-        courses.append(course)
-    return courses
 
+    page_num = 1
+    while True:
+        params = {
+            'page': page_num,
+            'page_size': page_size
+        }
+        response = requests.get(url, params=params)
+        data = response.json()
+        records = data['class_schedules']['records']
+        
+        if not records:
+            break
+        
+        for record in records:
+            course = Course(
+                subject=record[0],
+                catalog_number=record[1],
+                class_section=record[2],
+                class_number=record[3],
+                class_title=record[4],
+                class_topic_formal_desc=record[5],
+                instructor=record[6],
+                enrollment_capacity=record[7],
+                meeting_days=record[8],
+                meeting_time_start=record[9],
+                meeting_time_end=record[10],
+                term=record[11],
+                term_desc=record[12]
+            )
+            courses.append(course)
+
+        page_num += 1
+    
+    Course.objects.bulk_create(courses)
+
+    return courses
 
 def course_list(request):
     query = request.GET.get('q')
