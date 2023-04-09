f6d53b4d21673fbed48802e47bc5418cb62eb068
kjk5drb <123118453+kjk5drb@users.noreply.github.com>
2023-04-09 12:24:18 -0400

    fetch 15-19

--- myapp/views.py
+++ myapp/views.py
@@ -17,7 +17,7 @@ def fetch_courses():
     # courses = []
     url = 'https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearch?institution=UVA01&term=1232&acad_career=UGRD'
 
-    for x in range(10, 15):
+    for x in range(15, 20):
         data = requests.get(url + '&page=' + str(x))
         for c in data.json():
             if Course.objects.filter(sub_and_cat__icontains=c['subject'] + " " + c['catalog_nbr']):
